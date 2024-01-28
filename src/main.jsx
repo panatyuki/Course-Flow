8e5dfb814487dd93053260cc12184927a4415553
lacitadelle <jkomala99@gmail.com>
2024-01-28 23:55:16 +0700

    feat: added scaffold for authentication service

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
 import App from './App.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <MantineProvider>
+          <App/>
+        </MantineProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
