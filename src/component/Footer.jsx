3c397434fc5b701e593348f32840dbf5ed2100e1
noppadon <noppadonsangthongw@gmail.com>
2024-01-30 00:03:57 +0700

    feat: add footer in landing page

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,32 +1,61 @@
 import { useNavigate } from 'react-router-dom';
+import { Anchor, Group } from '@mantine/core';
 import classes from '../style/Footer.module.css';
-import courseFlowLogo from '../images/CourseFlow.png';
-import facebookLogo from '../images/facebookLogo.png';
-import instagramLogo from '../images/instagramLogo.png';
-import twitterLogo from '../images/twitterLogo.png';
+import teachLogo from '../images/imageTeach.svg';
+import polygon from '../images/polygon.svg';
+import ellipse from '../images/ellipse.svg';
+import courseFlowLogo from '../images/courseFlow.svg';
+import facebookLogo from '../images/facebookLogo.svg';
+import instagramLogo from '../images/instagramLogo.svg';
+import twitterLogo from '../images/twitterLogo.svg';
 
+const links = [
+  { link: '/', label: 'ALL Courses' },
+  { link: '/', label: 'Bundle Package' },
+];
 
 function Footer() {
   const navigate = useNavigate();
+  const items = links.map((link) => (
+    <Anchor
+      c="dimmed"
+      key={link.label}
+      lh={1}
+      onClick={() => navigate(`'${links.link}'`)}
+      size='xl'
+    >
+      {link.label}
+    </Anchor>
+  ));
 
   return (
     <>
-      <div className={classes.topFooter}>
-        <h2>Want to start learning?</h2>
-        <button className={classes.button}>
-          <p>Check out our courses</p>
-        </button>
+      <div className={classes.subFooter}>
+        <div className={classes.leftSubFooter}>
+          <h2>Want to start learning?</h2>
+          <button className={classes.button}>
+            <p>Check out our courses</p>
+          </button>
+        </div>
+        <img src={polygon} alt='polygon' width={42} height={42} className={classes.rightSubFooterPolygon} />
+        <img src={ellipse} alt='ellipse' width={26} height={26} className={classes.rightSubFooterEllipse} />
+        <img src={teachLogo} alt='teachLogo' width={592} height={448} className={classes.rightSubFooterTechLogo} />
       </div>
       <div className={classes.footer}>
-        <img src={courseFlowLogo} alt='courseFlowLogo' />
-        <div className={classes.textBoxCenter}>
-          <p>ALL Courses</p>
-          <p>Bundle Package</p>
-        </div>
-        <div>
-          <img src={facebookLogo} alt='facebookLogo' />
-          <img src={instagramLogo} alt='instagramLogo' />
-          <img src={twitterLogo} alt='twitterLogo' />
+        <img src={courseFlowLogo} alt='courseFlowLogo' className={classes.logoLink} onClick={() => {navigate('/');}} />
+        <Group >
+          {items}
+        </Group>
+        <div className={classes.logoBox}>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <img src={facebookLogo} alt='facebookLogo'/>
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <img src={instagramLogo} alt='instagramLogo'/>
+          </a>
+          <a href="https://twitter.com/?lang=en" target="_blank" rel="noopener noreferrer">
+            <img src={twitterLogo} alt='twitterLogo' />
+          </a>
         </div>
       </div>
     </>
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,6 +1,36 @@
+import classes from '../style/Main.module.css';
+import janeCooper from '../images/janeCooper.svg';
+import estherHoward from '../images/estherHoward.svg';
+import brooklynSimmons from '../images/brooklynSimmons.svg';
+
+const mockDataProfessional = [
+  { picture: janeCooper , name: 'Jane Cooper', position: 'UX/UI Designer' },
+  { picture: estherHoward , name: 'Esther Howard', position: 'Program Manager' },
+  { picture: brooklynSimmons , name: 'Brooklyn Simmons', position: 'Software Engineer' }
+];
+
 function Main() {
   return (
-    <div></div>
+    <>
+      <div className={classes.containerProfessionalInstructors}>
+        <h2>Our Professional Instructors</h2>
+        <div className={classes.containerCard}>
+          {
+            mockDataProfessional.map((items) => {
+              return (
+                <div className={classes.card} key={items.name}>
+                  <img src={items.picture} alt={items.name} />
+                  <div>
+                    <h3 style={{ lineHeight: '0' }}>{items.name}</h3>
+                    <p className='cf-body-2' style={{ lineHeight: '0' ,color: 'var(--blue-400, #5483D0)' }}>{items.position}</p>
+                  </div>  
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    </> 
   );
 }
 
