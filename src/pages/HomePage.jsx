3c06a67884ebce60e480861a537f536733a8bf17
BOSS <bigboss1796@hotmail.com>
2024-01-30 15:06:02 +0700

    edit:added pointer and made class for button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,13 @@ import Header from '../component/Header';
 import Main from '../component/Main';
 import Footer from '../component/Footer';
 
-
 function HomePage() {
   return (
-    <div>
+    <>
       <Header />
       <Main />
       <Footer />
-    </div>
+    </>
   );
 }
 
